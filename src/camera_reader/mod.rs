cd9406dcf6b07330d04479b0c0a658bbe9721133
John Fercher <johnathanfercher22@gmail.com>
2017-12-25 21:36:51 -0200

    ok

--- src/camera_reader/mod.rs
+++ src/camera_reader/mod.rs
@@ -1,41 +1,65 @@
 extern crate opencv;
 
-use std::sync::mpsc::{Sender, Receiver};
-use std::sync::mpsc;
 use std::thread;
-use std::io;
-use std::fs::File;
-use std::marker;
-use std::io::prelude::*;
 
-use self::opencv::core;
-use self::opencv::highgui;
+use self::opencv::core::{Mat, absdiff, bitwise_and, Size, Point, Scalar};
+use self::opencv::highgui::{VideoCapture, imshow, wait_key};
+use self::opencv::imgproc::{threshold, CV_THRESH_BINARY, cvt_color, CV_RGB2GRAY, MORPH_RECT, get_structuring_element, erode};
 
 pub struct CameraReader;
+
 
 impl CameraReader {
     pub fn new() -> Self {
         return CameraReader
     }
 
-    pub fn loop_reading(index: i32, sender: Sender<core::Mat>) -> /*thread::JoinHandle<Result<String,String>>*/ () {
+    pub fn loop_reading(&self, index: i32) -> thread::JoinHandle<Result<String,String>> {
         let thread_camera_reader: thread::JoinHandle<Result<String,String>> = thread::spawn(move || {
-            let mut cam = try!(highgui::VideoCapture::device(index));
-            let opened = try!(highgui::VideoCapture::is_opened(&cam));
-            let mut frame = try!(core::Mat::new());
+            let mut cam = try!(VideoCapture::device(index));
+            let opened = try!(VideoCapture::is_opened(&cam));
+
+            let mut frame = try!(Mat::new());
+            let mut prev_frame = try!(Mat::new());
+            let mut current_frame = try!(Mat::new());
+
+            let mut d1 = try!(Mat::new());
+            let mut d2 = try!(Mat::new());
+            let mut motion = try!(Mat::new());
+            let mut mask = try!(Mat::new());
 
             if !opened {
                 return Err(String::from("Deu bosta"))
             }
 
             loop {
+                let mut kernel_ero = get_structuring_element(MORPH_RECT, Size{ width: 2, height: 2}, Point{x: 0, y: 0});
+
+                prev_frame = current_frame.clone().unwrap();
+                current_frame = frame.clone().unwrap();
+
                 try!(cam.read(&mut frame));
-                sender.send(frame);
+
+                cvt_color(&frame, &frame, CV_RGB2GRAY, 0);
+
+                absdiff(&prev_frame, &frame, &d1);
+                absdiff(&frame, &current_frame, &d2);
+
+                bitwise_and(&d1, &d2, &motion, &mask);
+                threshold(&motion, &motion, 35.0, 255.0, CV_THRESH_BINARY);
+                erode(&motion, &motion, &kernel_ero.unwrap(), Point{x: 0, y: 0}, 0, 0, Scalar{data: [0.0, 0.0, 0.0, 0.0]});
+
+                if try!(motion.size()).width > 0 {
+                    try!(imshow("asd", &mut motion));
+                }
+                if try!(wait_key(10)) == 27 {
+                    break;
+                }
             }
 
             Ok(String::from("Ok"))
         });
 
-        //thread_camera_reader
+        thread_camera_reader
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,10 @@ extern crate opencv;
 mod camera_finder;
 mod camera_reader;
 
-use std::sync::mpsc::{Sender, Receiver};
-use std::sync::mpsc;
-use camera_finder::CameraFinder;
 use camera_reader::CameraReader;
-use opencv::core;
 
 fn main() {
-    let mut camera_finder = CameraFinder::new();
-    camera_finder.find_cameras().unwrap();
-
-    let (sender, receiver): (Sender<core::Mat>, Receiver<core::Mat>) = mpsc::channel();
-    let mut camera_reader = CameraReader::new();
-    let thread_camera_reader = camera_reader.loop_reading(camera_finder.first_or_default_camera(), sender);
+    let camera_reader = CameraReader::new();
+    let thread_camera_reader = camera_reader.loop_reading(0);
+    thread_camera_reader.join();
 }
